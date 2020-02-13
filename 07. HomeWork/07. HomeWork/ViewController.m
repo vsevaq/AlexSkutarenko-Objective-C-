1c7976c9ce404df5e04ae554e8892faeb6a32e74
Vsevolod <vsevolodq@gmail.com>
2020-02-13 11:19:48 +0200

    Initial point

---
+++ 07. HomeWork/07. HomeWork/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  07. HomeWork
+//
+//  Created by MacBook Pro on 2/13/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
