838b94fa2cd1fb0f8226cc16dfafbb2fa8526aa6
Vsevolod <vsevolodq@gmail.com>
2020-02-13 18:36:42 +0200

    Initial progect for 09. Delegates

---
+++ 09. Delegates/09. Delegates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  09. Delegates
+//
+//  Created by MacBook Pro on 2/13/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
