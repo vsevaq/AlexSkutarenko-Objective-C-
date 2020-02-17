aad110181277d5a8b6adccf382edb0ae5508ad31
Vsevolod <vsevolodq@gmail.com>
2020-02-17 17:28:09 +0200

    Starting lesson 11. Selectors

---
+++ 11. Selectors/11. Selectors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  11. Selectors
+//
+//  Created by MacBook Pro on 2/17/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 11. Selectors/11. Selectors/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  11. Selectors
+//
+//  Created by MacBook Pro on 2/17/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
