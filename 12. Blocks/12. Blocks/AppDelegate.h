99553511d051ae2befa08ab6ba4392b4f517d864
Vsevolod <vsevolodq@gmail.com>
2020-02-18 14:14:36 +0200

    Starting lesson 12.Blocks

---
+++ 12. Blocks/12. Blocks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  12. Blocks
+//
+//  Created by MacBook Pro on 2/18/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 12. Blocks/12. Blocks/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  12. Blocks
+//
+//  Created by MacBook Pro on 2/18/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
