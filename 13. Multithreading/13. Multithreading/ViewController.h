ed39e49d90e13855ff21765120d71562c041517a
Vsevolod <vsevolodq@gmail.com>
2020-02-21 16:33:21 +0200

    Initial point for 13.Multithreading lesson

---
+++ 13. Multithreading/13. Multithreading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  13. Multithreading
+//
+//  Created by MacBook Pro on 2/21/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
