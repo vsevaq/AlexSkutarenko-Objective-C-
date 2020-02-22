b5340f52feb7333ae5d09402c8d76db52310e7f8
Vsevolod <vsevolodq@gmail.com>
2020-02-22 16:48:18 +0200

    Initial progect for 14.NSString lesson

---
+++ 14. NSString/14. NSString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  14. NSString
+//
+//  Created by MacBook Pro on 2/22/20.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
